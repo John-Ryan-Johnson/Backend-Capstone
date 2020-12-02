44259d74ee02dd26e611130cd2d9ea75ec07a018
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-12-02 13:47:35 -0600

    added methods to controller and repo of employee, installed Dapper and Microsoft.Data.SqlClient

--- Supers_Choice/Controllers/EmployeesController.cs
+++ Supers_Choice/Controllers/EmployeesController.cs
@@ -2,15 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Supers_Choice.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Supers_Choice.Data;
 
 namespace Supers_Choice.Controllers
 {
-    public class EmployeesController : Controller
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
     {
-        public IActionResult Index()
+        EmployeeRepository _repo;
+
+        public EmployeesController()
         {
-            return View();
+            _repo = new EmployeeRepository();
+        }
+
+        [HttpPost]
+        public IActionResult CreateEmployee(Employee employee)
+        {
+            _repo.Add(employee);
+
+            return Created($"/api/employees/{employee.Id}", employee);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = _repo.GetAll();
+
+            return Ok(allEmployees);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetEmployeeById(int id)
+        {
+            var employee = _repo.GetById(id);
+
+            if (employee == null) return NotFound("No employee with that Id was found");
+
+            return Ok(employee);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            if (_repo.GetById(id) == null)
+            {
+                return NotFound();
+            }
+
+            _repo.Remove(id);
+
+            return Ok();
         }
     }
 }
--- Supers_Choice/Data/EmployeeRepository.cs
+++ Supers_Choice/Data/EmployeeRepository.cs
@@ -2,10 +2,70 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Supers_Choice.Models;
 
 namespace Supers_Choice.Data
 {
     public class EmployeeRepository
     {
+        static List<Employee> _employees = new List<Employee>();
+
+        const string _connectionString = "Server=localhost;Database=SupersChoice;Trusted_Connection=True;";
+
+        public void Add(Employee employeeToAdd)
+        {
+            var sql = @"INSERT INTO [dbo].[Customers]
+                               ([firstName]
+                               ,[lastName]
+                               ,[isSupervisor]
+                               ,[isManager]
+                                [isDeleted])
+                        Output inserted.id
+                        VALUES
+                               (@firstName,@lastName,@isSupervisor,@isManager,@isDeleted)";
+
+            using var db = new SqlConnection(_connectionString);
+
+            var newId = db.ExecuteScalar<int>(sql, employeeToAdd);
+
+            employeeToAdd.Id = newId;
+        }
+
+        public List<Employee> GetAll()
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var employees = db.Query<Employee>("select * from employees");
+
+            return employees.ToList();
+        }
+
+        public Employee GetById(int employeeId)
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var query = @"select *
+                          from Employees
+                          where id = @cid";
+
+            var parameters = new { cid = employeeId };
+
+            var employee = db.QueryFirstOrDefault<Employee>(query, parameters);
+
+            return employee;
+        }
+
+        public void Remove(int employeeId)
+        {
+            var sql = @"DELETE 
+                        FROM [dbo].[Employees]
+                        WHERE Id = @id";
+
+            using var db = new SqlConnection(_connectionString);
+
+            db.Execute(sql, new { id = employeeId });
+        }
     }
 }
