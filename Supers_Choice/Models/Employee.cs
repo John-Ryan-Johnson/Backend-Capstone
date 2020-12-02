f9e60648bcc3cb7a76cc40df55e751f7d7707bb8
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-12-02 13:58:57 -0600

    WIP

--- Supers_Choice/Models/Employee.cs
+++ Supers_Choice/Models/Employee.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Supers_Choice.Models
+﻿namespace Supers_Choice.Models
 {
     public class Employee
     {
