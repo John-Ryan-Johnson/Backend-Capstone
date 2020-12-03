cbd2c4e7755f07220f36d2b45e6789852eccb5fa
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-12-02 18:36:26 -0600

    created machine model, repo and controller

---
+++ Supers_Choice/Data/MachineRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Supers_Choice.Models;
+
+namespace Supers_Choice.Data
+{
+    public class MachineRepository
+    {
+        static List<Machine> _machines = new List<Machine>();
+
+        const string _connectionString = "Server=localhost;Database=SupersChoice;Trusted_Connection=True;";
+
+        public void Add(Machine machineToAdd)
+        {
+            var sql = @"INSERT INTO [dbo].[Customers]
+                               ([name]
+                               ,[employeeId]
+                               ,[isDeleted]
+                               ,[isRunning]
+                               ,[Date]
+                                [downtimeCodeId])
+                        Output inserted.id
+                        VALUES
+                               (@name,@employeeId,@isDeleted,@isRunning,@Date,@downtimeCodeId)";
+
+            using var db = new SqlConnection(_connectionString);
+
+            var newId = db.ExecuteScalar<int>(sql, machineToAdd);
+
+            machineToAdd.Id = newId;
+        }
+
+        public List<Machine> GetAll()
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var machines = db.Query<Machine>("select * from machines");
+
+            return machines.ToList();
+        }
+
+        public Machine GetById(int machineId)
+        {
+            using var db = new SqlConnection(_connectionString);
+
+            var query = @"select *
+                          from Machines
+                          where id = @mid";
+
+            var parameters = new { mid = machineId };
+
+            var machine = db.QueryFirstOrDefault<Machine>(query, parameters);
+
+            return machine;
+        }
+
+        public void Remove(int machineId)
+        {
+            var sql = @"DELETE 
+                        FROM [dbo].[Machines]
+                        WHERE Id = @id";
+
+            using var db = new SqlConnection(_connectionString);
+
+            db.Execute(sql, new { id = machineId });
+        }
+    }
+}
---
+++ Supers_Choice/Models/Machine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Supers_Choice.Models
+{
+    public class Machine
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int employeeId { get; set; }
+        public bool isDeleted { get; set; }
+        public bool isRunning { get; set; }
+        public DateTime Date { get; set; }
+        public int downtimeCodeId { get; set; }
+    }
+}
