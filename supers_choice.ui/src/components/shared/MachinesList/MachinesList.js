79a6777e0ebad1bcf05bd494289ab8510cad72d1
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-12-14 20:13:57 -0600

    machine list is printing

--- supers_choice.ui/src/components/shared/MachinesList/MachinesList.js
+++ supers_choice.ui/src/components/shared/MachinesList/MachinesList.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './MachinesList.scss';
+import { Link } from 'react-router-dom';
+import machineShape from '../../../helpers/propz/machineShape';
 
 class MachinesList extends React.Component {
+  static propTypes = {
+    machine: machineShape.machineShape,
+  }
   render() {
+    const { machine } = this.props;
+    const singleMachineLink = `/machine/${machine.employeeId}`
     return (
       <div className="MachinesList">
-        <h1>Machines List</h1>
+        <li className="list-group-item">
+          <Link to={singleMachineLink}><h3 className="item-name">{machine.name}</h3></Link>
+        </li>
       </div>
     );
   }
